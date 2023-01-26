970046aa2b55d1189860f0cf4ca18e216e14d875
Ali Arain <deistali2@gmail.com>
2023-01-26 22:50:53 +0500

    config for appwrite and svgs added

---
+++ lib/constants/appwrite.dart
@@ -0,0 +1,5 @@
+class AppWriteConstant {
+  static const String databaseID = '63d2bb8911e26e6e4731';
+  static const String projectID = '63d2b45333e0b03c16ae';
+  static const String endPoint = 'http://localhost:80/v1';
+}
