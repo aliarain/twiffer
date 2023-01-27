0cc9ff327e6b3cdb6d28bf683c5064298fe24de0
Ali Arain <deistali2@gmail.com>
2023-01-27 15:39:26 +0500

    Config For Design System

---
+++ lib/constants/ui_const.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:twiffer/constants/assets.dart';
+import 'package:twiffer/theme/pallete.dart';
+
+class UIConst {
+  static AppBar appbar() {
+    return AppBar(
+      title: SvgPicture.asset(AssetsConstants.twitterLogo,
+          color: Pallete.blueColor),
+    );
+  }
+}
