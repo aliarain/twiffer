99505b413dfab2dd082ff1ccf819593efb633561
Ali Arain <deistali2@gmail.com>
2023-01-28 19:46:28 +0500

    Auth App write

---
+++ lib/apis/authentication.dart
@@ -0,0 +1,35 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:twiffer/core/core.dart';
+
+final authAPIProvider = Provider((ref) {});
+
+abstract class IAuthAPI {
+  FutureEither<model.Account> signUp({
+    required String email,
+    required String password,
+  });
+}
+
+class AuthAPI implements IAuthAPI {
+  final Account _account;
+  AuthAPI({required Account account}) : _account = account;
+
+  @override
+  FutureEither<model.Account> signUp(
+      {required String email, required String password}) async {
+    try {
+      final account = await _account.create(
+          userId: ID.unique(), email: email, password: password);
+      return right(account);
+    } on AppwriteException catch (e, stackTrace) {
+      return left(
+          Failure(e.message ?? 'Some Unexpected error occured', stackTrace));
+    } catch (e, stackTrace) {
+      return left(Failure(e.toString(), stackTrace));
+      print(e);
+    }
+  }
+}
